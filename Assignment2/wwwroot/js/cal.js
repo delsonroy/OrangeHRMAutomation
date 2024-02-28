90d99a0446b0c5dc1bdc4f937e29bb5c623bc6e1
user <delson.geo@gmail.com>
2024-02-28 14:38:08 -0500

    modified the task

---
+++ Assignment2/wwwroot/js/cal.js
@@ -0,0 +1,34 @@
+﻿
+function add()
+{
+    let x = document.getElementById("value1").value
+    let y = document.getElementById("value2").value;
+    document.getElementById("output").value = parseInt(x) + parseInt(y);
+}
+
+
+function minus() {
+    let x = document.getElementById("value1").value
+    let y = document.getElementById("value2").value;
+    document.getElementById("output").value = parseInt(x) - parseInt(y);
+}
+
+function multiplication() 
+    {
+        let x = document.getElementById("value1").value
+        let y = document.getElementById("value2").value;
+        document.getElementById("output").value = parseInt(x) * parseInt(y);
+}
+
+
+function division() {
+    let x = document.getElementById("value1").value
+    let y = document.getElementById("value2").value;
+    document.getElementById("output").value = parseInt(x) / parseInt(y);
+}
+
+function mod() {
+    let x = document.getElementById("value1").value
+    let y = document.getElementById("value2").value;
+    document.getElementById("output").value = parseInt(x) % parseInt(y);
+}
